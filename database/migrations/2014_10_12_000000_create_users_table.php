252fdb0c70deed44bfb2adda66060c1c21c84aa4
Sahil Chahal <dev.sahilchahal@gmail.com>
2022-08-21 17:33:08 +0530

    Updated User migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('fname');
+            $table->string('lname')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('superadmin')->default(false);
+            $table->timestamp('dob')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
